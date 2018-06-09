4f25fbdcdc816394a128009e3d63c7070d7fd9c2
Aurélien Roux <aureroux13@me.com>
2018-06-09 17:14:16 -0400

    xhr request

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('test');
+var get = (url) => {
+    var xhr = new XMLHttpRequest()
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+            console.log(xhr.responseText);
+
+        }
+    }
+    xhr.open('GET', url, true)
+    xhr.send()
+}
+
+var getPosts = () => {
+    var users = get('http:/jsonplaceholder.typicode.com/users')
+}
+
+console.log(getPosts());
+
+
