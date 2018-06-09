a2cca0d7abdb73c1861a8a7d293f2b47c02642f0
Aurélien Roux <aureroux13@me.com>
2018-06-09 18:42:31 -0400

    promise on users functional

--- promises.js
+++ promises.js
@@ -16,28 +16,32 @@ var get = (url) => {
     })
 }
 
-get('http:/jsonplaceholder.typicode.com/users').then(function (response) {
+/* get('http:/jsonplaceholder.typicode.com/users')
+.then(function (response) {
     console.log(response);
-})
+}).catch(function (error) {
+    console.log(error);
+}) */
 
-
-/* var getPosts = (success, error) => {
-    get('http:/jsonplaceholder.typicode.com/users', function(response) {
+var getPosts = (success, error) => {
+    get('http:/jsonplaceholder.typicode.com/users').then(function (response) {
         var users = JSON.parse(response)
-        console.log(users);
-        get('http:/jsonplaceholder.typicode.com/commensssts?usersId=' + users[0].id, function(response) {
-            var posts = JSON.parse(response)
-            success(posts)
-        }, function(e) {
-            console.log('ajax error', e);
-        })
-    }, function(e) {
-        console.log('ajax error', e);
+        console.log(users[0]);
+        /*  get('http:/jsonplaceholder.typicode.com/comments?usersId=' + users[0].id, function (response) {
+             var posts = JSON.parse(response)
+             success(posts)
+         }, function (e) {
+             console.log('ajax error', e);
+         }) */
+    }).catch(function (e) {
+        console.log('users error', e);
     })
 }
 
-getPosts(function(posts) {
+getPosts();
+
+/* getPosts(function (posts) {
     console.log('premier', posts[0]);
-}, function(error) {
+}, function (error) {
     console.log(error);
 }); */
