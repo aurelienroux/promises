f38295ee900a29fbf560e4f209dc5b1829c616d7
Aurélien Roux <aureroux13@me.com>
2018-06-09 17:41:15 -0400

    callback with err handling

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var get = (url, success) => {
+var get = (url, success, error) => {
     var xhr = new XMLHttpRequest()
 
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4) {
-            success(xhr.responseText);
+            if (xhr.status === 400) {
+                success(xhr.responseText);
+            } else {
+                error(xhr.status)
+            }
         }
     }
     xhr.open('GET', url, true)
@@ -12,7 +16,12 @@ var get = (url, success) => {
 
 var getPosts = () => {
     get('http:/jsonplaceholder.typicode.com/users', function(response) {
-        console.log(response);
+        console.log('this is success');
+        var users = JSON.parse(response)
+        console.log(users[0]);
+    }, function(error) {
+        console.log('this is error');
+        console.log(error);
     })
 }
 
