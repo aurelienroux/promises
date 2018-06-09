478e7fe91048d744ad56767acb7ddadcdb046f23
Aurélien Roux <aureroux13@me.com>
2018-06-09 17:44:00 -0400

    fix xhr status code

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var get = (url, success, error) => {
 
     xhr.onreadystatechange = () => {
         if (xhr.readyState == 4) {
-            if (xhr.status === 400) {
+            if (xhr.status === 200) {
                 success(xhr.responseText);
             } else {
-                error(xhr.status)
+                error(xhr)
             }
         }
     }
@@ -15,7 +15,7 @@ var get = (url, success, error) => {
 }
 
 var getPosts = () => {
-    get('http:/jsonplaceholder.typicode.com/users', function(response) {
+    get('http:/jsonplaceholder.typicode.com/usersss', function(response) {
         console.log('this is success');
         var users = JSON.parse(response)
         console.log(users[0]);
